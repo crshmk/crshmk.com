5b889836ff13ebf80dbe8d67e2858812bff0ef58
Chris Hammock <chris@crshmk.com>
2021-04-08 20:09:58 -0500

    add github button

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import './home.css'
 
-let Home = () => {
+import GithubButton from './GithubButton'
 
+let linkToThisRepo = 'https://github.com/crshmk/crshmk.com'
+
+let Home = () => {
   return (
     <div className="home">
       <h1>Chris Hammock</h1>
       <h3>Web Developer</h3>
+      <GithubButton url={linkToThisRepo} />
     </div>
   )
 }
