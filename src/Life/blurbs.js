9722017428a0681990dff32c7d614c633e9825d4
Chris Hammock <chris@crshmk.com>
2021-04-09 12:39:09 -0500

    add life page

---
+++ src/Life/blurbs.js
@@ -0,0 +1,17 @@
+let past = "After undergrad, I lived outside of the US for 14 years. During that time I visited about 40 countries and worked in 5. Vietnam was home for ten years"
+
+let edCareer = "My first career was in private international education. I was a teacher, administrator, and finally consultant in Korea, Nepal, Myanmar, Saudi Arabia, and Vietnam. I taught advanced history at international schools and developed academic programs. I wrote curricula, worked with accrediting organizations, hired, trained, and evaluated staff, pitched to investors, and aided in general policy. It was a highly rewarding career and I plan to be involved in education again in the future."
+
+let softwareCareer = "In 2014 I transitioned from from education to software. I initially worked as an independent freelancer. About a couple years in, a furniture factory hired me as a full time software engineer. I created a a B2B catalogue and a number of administrative tools to aid operations."
+
+let apple = "In 2018 I moved back to the US. Since then I've mostly been writing software at Apple as an on site contractor. When I arrived, there was a small MVP. When I left, we had a successful app in production with a large user base. I led the React development and the front end architecture. I also mentored junior developers new to React. It was a very successful project."
+
+let music = "I'm also a guitarist. I've gigged professionally for years."
+
+export default [
+  past,
+  edCareer,
+  softwareCareer,
+  apple,
+  music
+]
--- src/Life/index.js
+++ src/Life/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
 
-let Life = () => <p>life</p>
+import './life.css'
+
+import blurbs from './blurbs'
+
+import { map } from 'ramda'
+
+let makeBlurbs = map(blurb => <p>{blurb}</p>)
+
+let paragraphs = makeBlurbs(blurbs)
+
+let Life = () => (
+  <div className="life hide-menu-active">
+    {paragraphs}
+  </div>
+)
 
 export default Life
