65623df7de4390f51b439f53b3cbcf84cb759c8d
Chris Hammock <chris@crshmk.com>
2022-03-05 10:38:02 -1000

    hide main menu on escape key

--- src/Menu/Links/index.js
+++ src/Menu/Links/index.js
@@ -3,19 +3,34 @@ import { NavLink } from 'react-router-dom'
 
 import pages from 'App/pages'
 
+import useMenu from '../useMenu'
 import { map } from 'ramda'
 
-const makeLinks = map(({ label, path }) => (
-  <li key={label} aria-label={label}>
-    <NavLink 
-      exact
-      activeClassName="active-menu-item"
-      to={path} 
-    >
-      {label}
-    </NavLink>
-  </li>
-))
+const onKey = hideMenu => e => {
+  if(e.key === 'Escape') hideMenu()
+}
+
+const Link = ({ label, path }) => {
+  const { hideMenu } = useMenu()
+  const onKeyDown = onKey(hideMenu)
+  return (
+    <li key={label} aria-label={label}>
+      <NavLink 
+        exact
+        activeClassName="active-menu-item"
+        to={path} 
+        onKeyDown={onKeyDown}
+      >
+        {label}
+      </NavLink>
+    </li>
+  )
+
+}
+
+const makeLinks = map(({ label, path }) => {
+  return <Link label={label} path={path} />
+} )
 
 const links = makeLinks(pages)
 
