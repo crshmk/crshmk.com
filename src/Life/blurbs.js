08bd18a67f8ff99c3ed1cd73e0d59faf1038e51e
Chris Hammock <chris@crshmk.com>
2022-03-05 11:48:08 -1000

    fix softwareCareer life blurb typo

--- src/Life/blurbs.js
+++ src/Life/blurbs.js
@@ -2,7 +2,7 @@ const past = "After undergrad, I lived outside of the US for 14 years. During th
 
 const edCareer = "My first career was in private international education. I was a teacher, administrator, and finally consultant in Korea, Nepal, Myanmar, Saudi Arabia, and Vietnam. I taught advanced history at international schools and developed academic programs. I wrote curricula, worked with accrediting organizations, hired, trained, and evaluated staff, pitched to investors, and aided in general policy. It was a highly rewarding career and I plan to be involved in education again in the future."
 
-const softwareCareer = "In 2014 I transitioned from from education to software. I initially worked as an independent freelancer. About a couple years in, a furniture factory hired me as a full time software engineer. I created a a B2B catalogue and a number of administrative tools to aid operations."
+const softwareCareer = "In 2014 I transitioned from from education to software. I initially worked as an independent freelancer. About a couple years in, a furniture factory hired me as a full time software engineer. I created a B2B catalogue and a number of administrative tools to aid operations."
 
 const apple = "In 2018 I moved back to the US and took a contracting job as a Javascript Engineer at Apple. When I arrived, there was a small MVP. When I left, we had a successful app in production with a large user base. I led the React development and the front end architecture. I also mentored junior developers new to React. It was a very successful project. I've since continued to work as a senior Javascript engineer."
 
