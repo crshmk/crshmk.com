07bd6d31211ccdb8a87fbee084003740afcea880
Chris Hammock <chris@crshmk.com>
2022-03-05 19:43:25 -1000

    use gist for imperative team filter

--- src/Home/FilterCardPunchers.js
+++ src/Home/FilterCardPunchers.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const urlToRamdaRepl = 'https://ramdajs.com/repl/#?const%20headLens%20%3D%20lensIndex%280%29%0A%0Aconst%20updateKeys%20%3D%20keyTransform%20%3D%3E%20%0A%20%20pipe%28%0A%20%20%20%20toPairs%2C%0A%20%20%20%20map%28over%28headLens%2C%20keyTransform%29%29%2C%0A%20%20%20%20fromPairs%0A%20%20%29%0A%0Aconst%20x%20%3D%20%7B%0A%20%20oNe%3A%201%2C%0A%20%20tWo%3A%202%0A%7D%0A%0Aconst%20toLowerKeys%20%3D%20updateKeys%28toLower%29%0AtoLowerKeys%28x%29%0A%0Aconst%20mapToLowerKeys%20%3D%20map%28toLowerKeys%29%0AmapToLowerKeys%28%5Bx%2C%20x%5D%29%0A%20%0A'
+const urlToFpGist = 'https://gist.github.com/crshmk/b87db5c3dc79211ea4adec51310aa4e1'
 
 const LinkToRamdaRepl = () => (
-  <a href={urlToRamdaRepl} target="_blank">this</a>
+  <a href={urlToFpGist} target="_blank">this</a>
 )
 
 const FilterCardPunchers = () => (
