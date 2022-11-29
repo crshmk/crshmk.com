fd732191f5e6ba14e344e91caf06f05abf48df47
Chris Hammock <chris@crshmk.com>
2022-11-29 10:02:51 +0700

    update life page

--- src/Life/blurbs.js
+++ src/Life/blurbs.js
@@ -1,17 +1,11 @@
-const past = "After undergrad, I lived outside of the US for 14 years. During that time I visited about 40 countries and worked in 5. Vietnam was home for ten years."
+const edCareer = "My first career was in private international education. I was a teacher, administrator, and finally consultant in Korea, Nepal, Myanmar, Saudi Arabia, and Vietnam. I taught advanced history at international schools and developed academic programs. I wrote curricula, worked with accrediting organizations, hired, trained, and evaluated staff, and aided with general policy. It was an enjoyable and rewarding career."
 
-const edCareer = "My first career was in private international education. I was a teacher, administrator, and finally consultant in Korea, Nepal, Myanmar, Saudi Arabia, and Vietnam. I taught advanced history at international schools and developed academic programs. I wrote curricula, worked with accrediting organizations, hired, trained, and evaluated staff, pitched to investors, and aided in general policy. It was a highly rewarding career and I plan to be involved in education again in the future."
+const softwareCareer = "In 2014 I transitioned from education to software. I initially worked as an independent freelancer. In 2016 a furniture factory hired me as a full time software engineer. I created a B2B catalogue and a number of administrative tools to aid operations."
 
-const softwareCareer = "In 2014 I transitioned from education to software. I initially worked as an independent freelancer. About a couple years in, a furniture factory hired me as a full time software engineer. I created a B2B catalogue and a number of administrative tools to aid operations."
-
-const apple = "In 2018 I moved back to the US and took a contracting job as a Software Engineer at Apple. When I arrived, there was a small MVP. When I left, we had a successful app in production with a large user base. I led the React development and the front end architecture. I also mentored junior developers new to React. It was a very successful project. I've since continued to work as a senior Javascript Engineer."
-
-const music = "I'm also a guitarist. I've gigged professionally for years."
+const apple = "In 2018 I moved back to the US and took a contract position as a Software Engineer at Apple. When I arrived, there was a small proof of concept. When I left, we had a successful app in production with a large user base. I led the React development and the front end architecture. I also mentored junior developers new to web development. It was a very successful project. I've since continued as a Senior Javascript Engineer."
 
 export default [
-  past,
   edCareer,
   softwareCareer,
-  apple,
-  music
+  apple
 ]
