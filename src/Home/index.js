5600259e203fdd6f77609b02b8fde6c2007d9066
Chris Hammock <chris@crshmk.com>
2022-11-29 09:44:09 +0700

    remove home page tagline

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 import './home.css'
 
-import FilterCardPunchers from './FilterCardPunchers'
 import LinkToThisRepo from './LinkToThisRepo'
 
 const Home = () => (
   <div className="home hide-menu-active">
     <h1>Chris Hammock</h1>
     <h3>Software Engineer</h3>
-    <FilterCardPunchers />
     <LinkToThisRepo />
   </div>
 )
