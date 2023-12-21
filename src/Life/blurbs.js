60dc96678744fbfe205ff30dde4556a6612cbd4f
Chris Hammock <chris@crshmk.com>
2023-12-21 12:58:35 +0700

    update Life page

--- src/Life/blurbs.js
+++ src/Life/blurbs.js
@@ -2,7 +2,7 @@ const edCareer = "My first career was in private international education. I was 
 
 const softwareCareer = "In 2014 I transitioned from education to software. I initially worked as an independent freelancer. In 2016 a furniture factory hired me as a full time software engineer. I created a B2B catalogue and a number of administrative tools to aid operations."
 
-const apple = "In 2018 I moved back to the US and took a contract position as a Software Engineer at Apple. When I arrived, there was a small proof of concept. When I left, we had a successful app in production with a large user base. I led the React development and the front end architecture. I also mentored junior developers new to web development. It was a very successful project. I've since continued as a Senior Javascript Engineer."
+const apple = "In 2018 I moved back to the US and took a contract position as a Software Engineer at Apple. When I arrived, there was a small proof of concept. When I left, we had a successful app in production with a large user base. I led the React development and the front end architecture. I also mentored junior developers new to web development. It was a very successful project. I've since continued as a full stack engineer."
 
 export default [
   edCareer,
