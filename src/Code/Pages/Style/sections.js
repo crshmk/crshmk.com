144ae82ce24da209e82d0506b82b5afccf55b6e9
Chris Hammock <chris@crshmk.com>
2024-06-03 14:43:39 -0400

    update style page

--- src/Code/Pages/Style/sections.js
+++ src/Code/Pages/Style/sections.js
@@ -6,18 +6,26 @@ import GithubIcon from 'src/Icons/GithubIcon'
 const urlToUtilLibrary = 'https://github.com/crshmk/utils'
 const urlToPhp = 'https://github.com/crshmk/vue-php/blob/master/private/Http/Request.php'
 const urlToThisRepo = 'https://github.com/crshmk/crshmk.com/tree/master/src'
+const urlToTs = 'https://github.com/crshmk/react-form'
 
 const UtilLibrary = () => (
   <>
-  small, composable functions
+  using small, composable functions
   <GithubIcon url={urlToUtilLibrary}/>
   </>
 )
 
 const PhpItem = () => (
   <>
-  when necessary
+  My old PHP apps were heavily object oriented
   <GithubIcon url={urlToPhp}/>
+  </>
+)
+
+const TsItem = () => (
+  <>
+  When memoization prevented the use of context, TS effectively clarified this prop drilling
+  <GithubIcon url={urlToTs}/>
   </>
 )
 
@@ -30,16 +38,16 @@ const ReactItem = () => (
 
 export default [
   {
-    header: 'Declarative, functional Javascript',
+    header: 'I prefer declarative, functional, vanilla Javascript',
     items: [<UtilLibrary />]
   },
   {
-    header: 'OOP',
-    items: [<PhpItem />]
+    header: 'I also use OOP',
+    items: [<PhpItem />, <TsItem />]
   },
   {
-    header: 'State',
-    items: ['elegant state design']
+    header: 'A decade of elegant state design',
+    items: ['using many popular patterns']
   },
   {
     header: 'Small, clean React components',
