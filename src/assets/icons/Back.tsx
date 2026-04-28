24c45c54814cb32b686f73abdd07721cd9597eb7
Chris Hammock <chris@crshmk.com>
2026-04-28 19:53:13 -0400

    fix icon attribute names

--- src/assets/icons/Back.tsx
+++ src/assets/icons/Back.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const BackIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
     <polyline points="9 14 4 9 9 4"></polyline>
     <path d="M20 20v-7a4 4 0 0 0-4-4H4"></path>
   </svg>
